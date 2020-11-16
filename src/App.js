b4a36bf58c3ec8dab4eaec5cb7d557a969179f93
Michael Adrah <43325182+michaeldelali@users.noreply.github.com>
2020-11-16 19:02:17 +0000

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+//page for hats
+
+const hatsPage = ()=>(
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
+<switch>
+
+</switch>
 
 function App() {
   return (
     <div>
+      <switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={hatsPage}/>
+      </switch>
       <HomePage/>
     </div>
   );
